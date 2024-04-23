64bf7a5dde0ea2f3e114cb8d74ab2e0197182fcc
lexur <lostsunset69@gmail.com>
2024-04-23 23:02:06 +0800

    change text

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -30,7 +30,7 @@ const count = ref(0)
     <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
     in your IDE for a better DX
   </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <p class="read-the-docs">this is my first github pages with github action</p>
 </template>
 
 <style scoped>
